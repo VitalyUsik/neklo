ace2d428fc991611b35f794349f52370c396b2e1
vitalyusik <vitalyusik1989@gmail.com>
2024-07-02 11:16:21 -0300

    Fix package file

--- package.sh
+++ package.sh
@@ -8,12 +8,10 @@ if [ ! -d "lambda_package" ]; then
 fi
 
 # Install dependencies into the lambda_package directory
-# pip install -r requirements.txt --target=lambda_package
+pip install -r requirements.txt --target=lambda_package
+
+cp lambda_function.py lambda_package/lambda_function.py
 
 # Navigate to the lambda_package directory and create the zip package
 cd lambda_package
 zip -r ../../lambda.zip .
-cd ..
-
-# Add the lambda_function.py file to the zip package
-zip -g ../../lambda.zip lambda_function.py
